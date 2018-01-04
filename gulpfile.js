633e96fa1c4691a83cdcfd3ade951965ff344320
overdodactyl <pjohns33@protonmail.com>
2018-01-04 16:45:53 -0700

    Initial upload

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var gulp = require('gulp');
+var concatCss = require('gulp-concat-css');
+var cleanCSS = require('gulp-clean-css');
+
+
+gulp.task('minify-css', () => {
+  return gulp.src(['Stylus_Dark.css'])
+    .pipe(concatCss('Stylus_Dark.css'))
+    .pipe(cleanCSS({
+      level : 2 ,
+      format: 'beautify'
+    }))
+    .pipe(gulp.dest('.'));
+});
